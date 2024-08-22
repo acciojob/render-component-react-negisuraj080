c3a7b6c06066b8eb4748be106ad3491665b91a24
suraj negi <negisuraj080@gmail.com>
2024-08-22 11:23:09 +0530

    commit 3

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
+      <h1>Welcome to My React App</h1>
       <p>I am learning React. My life is getting better.</p>
     </div>
   );
